bbf71f59897e896ec1805bf8897bf7f8784f8329
maxzhang686 <maxzhang686@gmail.com>
2020-03-15 23:46:59 +1100

    selection dom, add style. creat dom and inser it.

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ console.log(sumUp(1, 2, 3, 4));
 //output: 36
  */
 
-
+/* 
 const sumUp = (resultHandler, ...number) => {
   let sum = 0;
   for (const num of number ){
@@ -66,3 +66,55 @@ const showResult = ( result) => {
 }
 
 sumUp(showResult, 1, 2, 3, 4);
+
+ */
+/* 
+document.getElementById("id-name");
+
+document.getElementsByTagName("tab-name");
+document.querySelector("tab-name/.class-name/#id-name")
+//return the first one
+//document.getElementsByClassName("class-name");
+document.querySelectorAll("tab-name/.class-name")
+ */
+
+
+/* 
+const ul = document.querySelector('ul');
+ul.children; //show all elements (li) in the ul
+ul.children[1];// select the second element (li)
+ul.firstElementChild;
+
+ul.childNodes; //show all node in the ul
+document.documentElement.parentElement;//null
+document.documentElement.parentNode;//#document
+
+const ul = li.parentElement;
+
+ul.previousSibling; //text node
+ul.previousElementSibling; //<header></header>
+
+const section = document.querySelector('section');
+//example: 
+section.className = 'xxx'; //change the section css class-name to xxx
+section.classList.contains('xxx');//check is set or not.
+section.classList.add('xxx');//add class name.
+section.classList.toggle('xxx');//change set/unset
+ */
+
+// const ulList = document.querySelector('ul');
+// const p = document.querySelector('p');
+
+// p.textContent = "New content";
+// ulList.innerHTML = "<li>new li<li>";//rewrite 
+
+// ulList.innerHTML = ulList.innerHTML + "<li>add li<li>";//add
+// //not good for the performance. render other-element you don't want to change
+
+const list = document.querySelector('ul');
+const secondLi = list.children[1]
+
+const newLi = document.createElement('li');
+newLi.textContent = "Number 4";
+
+secondLi.insertAdjacentElement('afterend',newLi);
