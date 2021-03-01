3d9efe2b376f801a12d0a64fba846de2a9dad08c
maxzhang686 <maxzhang686@gmail.com>
2021-03-01 14:24:51 +1100

    intersectionObserve example

--- intersectionObserver/assets/app.js
+++ intersectionObserver/assets/app.js
@@ -1,36 +1,15 @@
-const rows = document.querySelectorAll("#ui ul li");
-// const rows = document.querySelectorAll(".test");
-const html = document.documentElement;
+const images = document.querySelectorAll(".anim");
 
-document.addEventListener("scroll", (e) => {
-  let scrolled = html.scrollTop / (html.scrollHeight - html.clientHeight);
-
-  let total = 1 / rows.length;
-
-  for (let [index, row] of rows.entries()) {
-    // let progress = (scrolled - start) / (end - start);
-    let progress =
-      html.clientHeight + html.scrollTop - row.offsetTop - row.offsetHeight;
-    // console.log(
-    //   html.clientHeight,
-    //   html.scrollTop,
-    //   row.offsetTop,
-    //   row.offsetHeight
-    // );
-    console.log(progress);
-
-    // if (progress >= 1) progress = 1;
-    // if (progress <= 0) progress = 0;
-
-    if (progress >= 1) {
-      row.classList.remove("content--title--hide");
-      row.classList.add("content--title");
+observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.intersectionRatio > 0) {
+      entry.target.style.animation = `anim1 2s ${entry.target.dataset.delay} forwards ease-out`;
+    } else {
+      entry.target.style.animation = "none";
     }
-    if (progress <= 0) {
-      row.classList.remove("content--title");
-      row.classList.add("content--title--hide");
-    }
+  });
+});
 
-    // row.style.setProperty("--progress", progress);
-  }
+images.forEach((image) => {
+  observer.observe(image);
 });
